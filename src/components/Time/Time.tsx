7306f7099eec68129fd95720b7cf1a45a1322cd0
Marina Diubailo <marinadiubailo@gmail.com>
2025-01-13 12:00:30 +0300

    IN-72 feat: add Time

---
+++ src/components/Time/Time.tsx
@@ -0,0 +1,38 @@
+import React, { forwardRef } from 'react'
+
+import clsx from 'clsx'
+import * as timeago from 'timeago.js'
+import ru from 'timeago.js/lib/lang/ru'
+import TimeAgo from 'timeago-react'
+
+import s from './Time.module.scss'
+
+import { Typography } from '../Typography/Typography'
+
+timeago.register('ru', ru)
+
+export type TimeProps = {
+  /** en by default*/
+  locale?: string
+  /** @format date example: '2022-10-25T12:00:00.000Z' */
+  time: string
+} & React.ComponentPropsWithoutRef<'span'>
+
+export const Time = forwardRef<HTMLSpanElement, TimeProps>(
+  ({ className, locale, time, ...rest }, ref) => {
+    return (
+      <Typography
+        as={'span'}
+        className={clsx(s.date, className)}
+        id={'time'}
+        ref={ref}
+        variant={'small-text'}
+        {...rest}
+      >
+        <TimeAgo datetime={time} locale={locale} />
+      </Typography>
+    )
+  }
+)
+
+Time.displayName = 'Time'
